12f5bca293ae42e395362f5aa51c5e8fda3ba156
Phong Kien <phongkien@gmail.com>
2019-12-18 20:02:31 -0500

    include var in fragment

--- src/main/java/com/example/thymefreakingleap/controller/HomeController.java
+++ src/main/java/com/example/thymefreakingleap/controller/HomeController.java
@@ -1,12 +1,14 @@
 package com.example.thymefreakingleap.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller()
 public class HomeController {
 	@GetMapping("/")
-	public String home() {
+	public String home(Model model) {
+		model.addAttribute("greeter", "James Buttock");
 		return "index";
 	}
 }
