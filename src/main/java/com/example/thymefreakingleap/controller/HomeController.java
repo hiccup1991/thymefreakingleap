705be78e40b55795ab2a13eeaf58b31db1224d09
Phong Kien <phongkien@gmail.com>
2019-12-19 13:25:37 -0500

    dynamic fragment

--- src/main/java/com/example/thymefreakingleap/controller/HomeController.java
+++ src/main/java/com/example/thymefreakingleap/controller/HomeController.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 	@GetMapping("/")
 	public String home(Model model) {
-		model.addAttribute("greeter", "James Buttock");
+		model.addAttribute("greeter", "James Buttocker");
+		model.addAttribute("option_path", "/foo/bar/options2.html");
 		return "index";
 	}
 }
