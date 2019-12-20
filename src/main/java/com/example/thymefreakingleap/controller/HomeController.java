f22b9ecf35c1ee086c24b42a11ea953aacff8515
Phong Kien <phongkien@gmail.com>
2019-12-19 20:40:07 -0500

    loop test

--- src/main/java/com/example/thymefreakingleap/controller/HomeController.java
+++ src/main/java/com/example/thymefreakingleap/controller/HomeController.java
@@ -1,8 +1,13 @@
 package com.example.thymefreakingleap.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.thymefreakingleap.model.Person;
 
 @Controller()
 public class HomeController {
@@ -10,6 +15,13 @@ public class HomeController {
 	public String home(Model model) {
 		model.addAttribute("greeter", "James Buttocker");
 		model.addAttribute("option_path", "/foo/bar/options2.html");
+		
+		List<Person> persons = new ArrayList<Person>();
+		persons.add(new Person("Homer Simpson", 56));
+		persons.add(new Person("Marge Simpson", 50));
+		
+		model.addAttribute("persons", persons);
+		
 		return "index";
 	}
 }
